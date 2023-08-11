04557f4de73c615c33a9b5effb0ac4a2255114a3
Imam <57792561+imam-db@users.noreply.github.com>
2023-08-11 15:19:41 +0700

    Time: 1455 ms (14.45%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -9,8 +9,9 @@ SELECT e1.Id,
 FROM Employee AS e1
 INNER JOIN Employee AS e2
 ON e1.ManagerId = e2.Id
+WHERE e1.Salary > e2.Salary
 )
 SELECT cte.Name AS Employee
 FROM cte
-WHERE cte.salary_manager IS NOT NULL
-AND cte.Salary > cte.salary_manager;
+-- WHERE cte.salary_manager IS NOT NULL
+-- AND cte.Salary > cte.salary_manager;
