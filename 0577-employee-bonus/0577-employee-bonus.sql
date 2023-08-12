de63a3c021ca9ef6bbb6e133b7eeda32038dbd02
Imam <57792561+imam-db@users.noreply.github.com>
2023-08-12 07:31:13 +0700

    Time: 2459 ms (21.85%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,17 +1,8 @@
 # Write your MySQL query statement below
 
-SELECT e.name, null AS bonus
+SELECT e.name, 
+       CASE WHEN b.bonus < 1000 THEN b.bonus ELSE null END AS bonus
 FROM Employee AS e
-WHERE NOT EXISTS
-(
-    SELECT 1
-    FROM Bonus AS b
-    WHERE b.empId = e.empId
-)
-UNION ALL
-SELECT e.name, b.bonus
-FROM Employee AS e
-INNER JOIN Bonus AS b
-ON e.empId = b.empId
-WHERE b.bonus < 1000
-    
+LEFT JOIN Bonus AS b 
+    ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
