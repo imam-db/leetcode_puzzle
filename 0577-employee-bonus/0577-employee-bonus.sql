c0ad53dfd5c74e56fec25bdd511d9257e078a8ed
Imam <57792561+imam-db@users.noreply.github.com>
2023-08-12 07:32:06 +0700

    Time: 2492 ms (20.1%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
 SELECT e.name, 
-       CASE WHEN b.bonus < 1000 THEN b.bonus ELSE null END AS bonus
+       b.bonus
 FROM Employee AS e
 LEFT JOIN Bonus AS b 
     ON e.empId = b.empId
