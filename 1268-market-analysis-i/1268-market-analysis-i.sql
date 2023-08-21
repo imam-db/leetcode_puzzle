553fe882278f5ae7a3c2021688deb1418ab93efe
Imam <57792561+imam-db@users.noreply.github.com>
2023-08-21 12:57:51 +0700

    Time: 2409 ms (84.51%), Space: 0B (100%) - LeetHub

--- 1268-market-analysis-i/1268-market-analysis-i.sql
+++ 1268-market-analysis-i/1268-market-analysis-i.sql
@@ -1,10 +1,16 @@
 # Write your MySQL query statement below
 
+WITH order_2019 AS
+(
+    SELECT *
+    FROM Orders
+    WHERE order_date BETWEEN '2019-01-01' AND '2019-12-31'
+)
+
 SELECT u.user_id AS buyer_id,
     u.join_date,
     COUNT(DISTINCT o.order_id) AS orders_in_2019
 FROM Users AS u
-LEFT JOIN Orders AS o
+LEFT JOIN order_2019 AS o
 ON u.user_id = o.buyer_id
-AND o.order_date BETWEEN  '2019-01-01' AND '2019-12-31'
 GROUP BY u.user_id
