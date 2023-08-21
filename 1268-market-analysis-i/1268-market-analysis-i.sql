eb3b9266cacdcbab81f10cfa5985571e644243c1
Imam <57792561+imam-db@users.noreply.github.com>
2023-08-21 12:58:14 +0700

    Time: 3631 ms (11.82%), Space: 0B (100%) - LeetHub

--- 1268-market-analysis-i/1268-market-analysis-i.sql
+++ 1268-market-analysis-i/1268-market-analysis-i.sql
@@ -2,15 +2,16 @@
 
 WITH order_2019 AS
 (
-    SELECT *
+    SELECT buyer_id,
+        COUNT(order_id) AS total_order
     FROM Orders
     WHERE order_date BETWEEN '2019-01-01' AND '2019-12-31'
+    GROUP BY buyer_id
 )
 
 SELECT u.user_id AS buyer_id,
     u.join_date,
-    COUNT(DISTINCT o.order_id) AS orders_in_2019
+    COALESCE(o.total_order,0) AS orders_in_2019 
 FROM Users AS u
 LEFT JOIN order_2019 AS o
 ON u.user_id = o.buyer_id
-GROUP BY u.user_id
